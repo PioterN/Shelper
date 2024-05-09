7680addebdd20debf2e276746636c45f7967d0a4
PioterN <piotrnoga104@gmail.com>
2024-05-09 20:28:47 +0200

    Utworzenie ustawień konta

--- client/src/components/views/AccountSettings.vue
+++ client/src/components/views/AccountSettings.vue
@@ -1,9 +1,31 @@
-<template>
-    <NavBar />
-    <h1>Ustawienia konta</h1>
-    <p>Ustawienia konta</p>
-</template>
-
 <script setup>
 import NavBar from "../NavBar.vue";
 </script>
+
+
+<template>
+    <div className="acc-home-div">
+      <NavBar />
+      <div className="acc-content-div">
+        <h2 className="title">Ustawienia konta</h2>
+        <div className="acc-input-div">
+          <div>
+            <span>E-mail:</span>
+            <input className="acc-input" type="text" value="" />
+          </div>
+          <div>
+            <span>Hasło:</span>
+            <input className="acc-input" type="text" value="" />
+          </div>
+          <div>
+            <span>Powtórz hasło:</span>
+            <input className="acc-input" type="text" value="" />
+          </div>
+        </div>
+        <button className={classes.button}>Zatwierdź zmiany</button>
+        <button className={classes.button} onClick={logout}>
+          Wyloguj
+        </button>
+      </div>
+    </div>
+</template>
